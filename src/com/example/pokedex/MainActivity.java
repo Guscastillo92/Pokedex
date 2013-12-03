956100a3dd0ac9d6c7953d1423f5e7497a793077
Gustavo Castillo <gcastillo@smu.edu>
2013-12-03 10:42:11 -0600

    Parses Pokemon JSON and displays names into a text box, Created a Pokemon object

--- src/com/example/pokedex/MainActivity.java
+++ src/com/example/pokedex/MainActivity.java
@@ -1,15 +1,63 @@
 package com.example.pokedex;
 
-import android.os.Bundle;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String pokemon = "didnt work";
         setContentView(R.layout.activity_main);
+        try {
+			pokemon = getPokemon();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        TextView main = (TextView) findViewById(R.id.textBox);
+        ArrayList <String> Pokemon = parsePokemon(pokemon);
+        for (int p = 0; p < Pokemon.size(); p++){
+        	main.append(Pokemon.get(p));
+        }
+    }
+    
+    public String getPokemon() throws IOException {
+        InputStream file = getAssets().open("Pokemon.json");
+
+        byte[] data = new byte[file.available()];
+        file.read(data);
+        file.close();
+        return new String(data);
+    }
+    public ArrayList<String> parsePokemon(String pkmnJSON){
+    	ArrayList<String> Pokemon = new ArrayList<String>();
+    	JSONArray jsonPkmn;
+    	try{
+    		jsonPkmn = new JSONArray(pkmnJSON);
+			for(int x = 0; x < jsonPkmn.length(); x++){
+				JSONObject temppkmn = jsonPkmn.getJSONObject(x);
+				Pokemon.add(temppkmn.getString("name"));
+			}
+
+    		
+    	}
+    	catch (JSONException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	return Pokemon;
     }
 
 
---
+++ src/com/example/pokedex/Pokemon.java
@@ -0,0 +1,20 @@
+package com.example.pokedex;
+
+import java.util.ArrayList;
+
+public class Pokemon {
+	String name;
+	int id;
+	String img;
+	ArrayList <String> type;
+	int[] stats;
+	
+	Pokemon (String n, int i, String url, ArrayList <String> t, int[] s){
+		name = n;
+		id = i;
+		img = url;
+		type = t;
+		stats = s;
+	}
+
+}
